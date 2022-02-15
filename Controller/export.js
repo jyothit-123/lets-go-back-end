eb4e44c18e92726e0daab4150284b77d34e68163
jyothit-123 <jyothikalluraya97@gmail.com>
2022-02-15 18:47:00 +0530

    Signup and login post calls

--- Controller/export.js
+++ Controller/export.js
@@ -1,5 +1,68 @@
 let mongoose=require("../Model/mongoose");
 
+exports.findAllUsers=async(req,res)=>{
+    try{
+        let allUserData=await mongoose.find();
+        if(allUserData.length>0){
+            res.status(200).json({
+                status:"Success",
+                data:allUserData
+            })
+        }else{
+            res.status(400).json({
+                status:"Failure",
+                message:"User data not found"
+            })
+        }
+    }catch(err){
+        res.status(400).json({
+            status:"Failure",
+            message:"Api call failure"
+        })
+    } 
+}
+
+exports.signup=async(req,res)=>{
+    let requestBody=req.body;
+    mongoose.create(requestBody).then(()=>{
+        res.status(200).json({
+            status:"Success",
+            message:"Login Success",
+            data:requestBody
+        })
+    }).catch(()=>{
+        res.status(400).json({
+            status:"Failure",
+            message:"Invalid data"
+        })
+    })
+}
+
+exports.login=async(req,res)=>{
+    try{
+        let loginData=req.body;
+        let login=await mongoose.findOne({emaiId:loginData.emaiId,password:loginData.password});
+        if(login){
+            res.status(200).json({
+                status:"Success",
+                data:loginData
+            })
+        }else{
+            res.status(400).json({
+                status:"Failure",
+                message:"Invalid login data"
+            })
+        }
+
+    }catch(err){
+        res.status(400).json({
+            status:"Failure",
+            message:"Login Api failure"
+        })
+    }
+}
+
+
 exports.invalid=async(req,res)=>{
     res.status(400).json({
         status:"Invalid",
--- Router/router.js
+++ Router/router.js
@@ -2,6 +2,9 @@ let express=require("express");
 let router=express.Router();
 let apiCalls=require('../Controller/export')
 
+router.post("/signup",apiCalls.signup);
+router.post("/login",apiCalls.login);
+router.get("/findAllUsers",apiCalls.findAllUsers);
 router.all("*",apiCalls.invalid);
 
 module.exports=router;
